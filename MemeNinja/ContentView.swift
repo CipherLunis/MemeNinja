8796907e8847c418b0226674180ff5d2fdacd58a
Cipher Lunis <cipheryxinquiries@gmail.com>
2024-08-23 21:18:23 -0500

    Initial Commit

---
+++ MemeNinja/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemeNinja
+//
+//  Created by Cipher Lunis on 8/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemeNinja/MemeNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeNinjaApp.swift
+//  MemeNinja
+//
+//  Created by Cipher Lunis on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MemeNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
